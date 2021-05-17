666ac2b5933a3feacf4ef54873261b0fbab0170f
RetroSpectro <on_desk@mail.ru>
2021-05-17 03:04:13 +0300

    init commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1075, 518);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 566);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(563, 553);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "JK";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(563, 578);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "T";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(563, 604);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(33, 19);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "D";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Qt";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Qt+1";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "trig1";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "trig2";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "trig3";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "trig4";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "trig5";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "trig6";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "trig7";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "trig8";
+            this.Column10.Name = "Column10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 706);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public string type;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            type = "JK";
+            radioButton1.Checked = true;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "JK";
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "T";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "D";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (type == "JK")
+            {
+                for (int i = 0; i < dataGridView1.RowCount-1; i++)
+                {
+                    string str1 = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string str2 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    char[] s1 = str1.ToCharArray();
+                    char[] s2 = str2.ToCharArray();
+                    for (int j = 0; j <= s1.Length-1; j++)
+                    {
+                        int jj = j;
+                        if(s1[j] =='0'&&s2[j] =='0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "0/-";
+                        }
+                        if (s1[j] == '0' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "1/-";
+                        }
+                        if (s1[j] == '1' && s2[j] == '0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = " -/1";
+                        }
+                        if (s1[j] == '1' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "-/0";
+                        }
+
+                    }
+                }
+               
+            }
+            if (type == "T")
+            {
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    string str1 = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string str2 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    char[] s1 = str1.ToCharArray();
+                    char[] s2 = str2.ToCharArray();
+                    for (int j = 0; j < s1.Length; j++)
+                    {
+                        if (s1[j] == '0' && s2[j] == '0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "0";
+                        }
+                        if (s1[j] == '0' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "1";
+                        }
+                        if (s1[j] == '1' && s2[j] == '0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "1";
+                        }
+                        if (s1[j] == '1' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "0";
+                        }
+                    }
+                }
+            }
+            if (type == "D")
+            {
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    string str1 = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string str2 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    char[] s1 = str1.ToCharArray();
+                    char[] s2 = str2.ToCharArray();
+                    for (int j = 0; j < s1.Length; j++)
+                    {
+                        if (s1[j] == '0' && s2[j] == '0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "0/-";
+                        }
+                        if (s1[j] == '0' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "1/-";
+                        }
+                        if (s1[j] == '1' && s2[j] == '0')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "0";
+                        }
+                        if (s1[j] == '1' && s2[j] == '1')
+                        {
+                            dataGridView1.Rows[i].Cells[j + 2].Value = "1";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
